2626bd64ce0efdd7f85fc75cf7d41a28b2475d72
Jefferson Rodrigues de Oliveira <40842071+Dev-Jeff@users.noreply.github.com>
2018-12-07 21:17:03 -0200

    Modificações em Aluguel.java
    Adicionando Id para aluguel e modificando nome de colunas.

--- src/java/br/com/fatepg/bikeUp/Aluguel.java
+++ src/java/br/com/fatepg/bikeUp/Aluguel.java
@@ -3,7 +3,7 @@ package br.com.fatepg.bikeUp;
 import java.util.ArrayList;
 
 public class Aluguel {
-
+    private int idAluguel;
     private int idBike;
     private double vlTotal;
     private String tipoPagamento;
@@ -11,7 +11,8 @@ public class Aluguel {
       private String hrRetirado;
       private String hrDevolucao;
 
-    public Aluguel(int idBike,double vlTotal,String tipoPagamento, String loginUsuario, String hrRetirado,String hrDevolucao) {
+    public Aluguel(int idAluguel, int idBike,double vlTotal,String tipoPagamento, String loginUsuario, String hrRetirado,String hrDevolucao) {
+        this.idAluguel = idAluguel;
         this.idBike = idBike;
         this.hrRetirado = hrRetirado;
         this.vlTotal = vlTotal;
@@ -19,7 +20,13 @@ public class Aluguel {
         this.tipoPagamento = tipoPagamento;
         this.loginUsuario = loginUsuario;
     }
-
+    public int getIdAluguel(){
+        return idAluguel;
+    }
+    
+    public void setIdAluguel(int idAluguel){
+        this.idAluguel = idAluguel;
+    }
     public int getIdBike() {
         return idBike;
     }
@@ -68,21 +75,21 @@ public class Aluguel {
         this.loginUsuario = loginUsuario;
     }
 
-    public static void addAluguel(int idBike, String hrRetirado, String loginUsuario) throws Exception {
-        String SQL = "INSERT INTO ALUGUEL (FK_ID_BIKE, HR_RETIRADO, VL_TOTAL, HR_DEVOLUCAO, TP_PAGAMENTO, FK_NM_LOGIN)"
-                + "VALUES (?,?,0,'00:00:00','',?)";
-        Object parameters[] = {idBike, hrRetirado, loginUsuario};
+    public static void addAluguel(int idAluguel, int idBike, String hrRetirado, String loginUsuario) throws Exception {
+        String SQL = "INSERT INTO ALUGUEL (ID_ALUGUEL, ID_BIKE, HR_RETIRADO, VL_TOTAL, HR_DEVOLUCAO, TP_PAGAMENTO, NM_LOGIN)"
+                + "VALUES (?,?,?,0,'00:00:00','',?)";
+        Object parameters[] = {idAluguel, hrRetirado, loginUsuario};
         DataBaseConnector.execute(SQL, parameters);
     }
 
-    public static void editAluguel(int idBike, String hrDevolucao, double valorTotal, String tipoPagamento, String loginUsuario) throws Exception {
-        String SQL = "UPDATE ALUGUEL SET HR_DEVOLUCAO = ?, VL_TOTAL = ?, TP_PAGAMENTO = ? WHERE FK_ID_BIKE = ? AND FK_NM_LOGIN = ?";
-        Object parameters[] = {hrDevolucao, valorTotal, tipoPagamento, idBike, loginUsuario};
+    public static void editAluguel(int idAluguel, String hrDevolucao, double valorTotal, String tipoPagamento, String loginUsuario) throws Exception {
+        String SQL = "UPDATE ALUGUEL SET HR_DEVOLUCAO = ?, VL_TOTAL = ?, TP_PAGAMENTO = ? WHERE ID_ALUGUEL = ? AND FK_NM_LOGIN = ?";
+        Object parameters[] = {hrDevolucao, valorTotal, tipoPagamento, idAluguel, loginUsuario};
         DataBaseConnector.execute(SQL, parameters);
     }
 
     public static ArrayList<Aluguel> getAluguelUsuario(String login) throws Exception {
-        String SQL = "SELECT a.fk_id_bike,a.hr_retirado,a.vl_total,a.hr_devolucao, a.tp_pagamento, a.fk_nm_login from aluguel a, usuario u where a.fk_nm_login = u.nm_login and u.nm_login = ?";
+        String SQL = "SELECT a.id_aluguel,a.id_bike,a.hr_retirado,a.vl_total,a.hr_devolucao, a.tp_pagamento, a.nm_login from aluguel a, usuarios u where a.nm_login = u.nm_login and u.nm_login = ?";
         Object parameters[] = {login};
         ArrayList<Aluguel> aluguel = new ArrayList<>();
         ArrayList<Object[]> list = DataBaseConnector.getQuery(SQL, parameters);
@@ -90,33 +97,35 @@ public class Aluguel {
             Object row[] = list.get(i);
             Aluguel a = new Aluguel(
                     (int) row[0],
-                    (double) row[1],
-                    (String) row[2],
+                    (int) row[1],
+                    (double) row[2],
                     (String) row[3],
                     (String) row[4],
-                    (String) row[5]);
+                    (String) row[5],
+                    (String) row[6]);
             aluguel.add(a);
         }
         return aluguel;
     }
 
     public static ArrayList<Aluguel> getAluguel() throws Exception {
-        String SQL = "SELECT * "
-                + "from aluguel";
+        String SQL = "SELECT * FROM ALUGUEL";
+               
         ArrayList<Aluguel> aluguel = new ArrayList<>();
         ArrayList<Object[]> list = DataBaseConnector.getQuery(SQL, new Object[]{});
         for (int i = 0; i < list.size(); i++) {
             Object row[] = list.get(i);
             Aluguel a = new Aluguel(
                     (int) row[0],
-                    (double) row[1],
-                    (String) row[2],
+                    (int) row[1],
+                    (double) row[2],
                     (String) row[3],
                     (String) row[4],
-                    (String) row[5]
+                    (String) row[5],
+                    (String) row[6]
             );
             aluguel.add(a);
-        };
+        }
         return aluguel;
     }
 
